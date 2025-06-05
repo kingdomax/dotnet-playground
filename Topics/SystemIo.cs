1cf00c1aaa12afa54f14522cc8f0b06444538ff8
kingdomax <kingdomax@windowslive.com>
2025-06-05 16:08:45 +0200

    few fix

--- Topics/SystemIo.cs
+++ Topics/SystemIo.cs
@@ -26,14 +26,17 @@ namespace Playground.Topics
             string combinedContent = $"{DateTime.Now} \n";
             foreach (var textFile in textFileToProcess)
             {
-                combinedContent += $"[{Path.GetFileName(textFile)}]: ";
+                var fileName = Path.GetFileName(textFile);
+                if (fileName == "combined_text.txt") { continue; }
+
+                combinedContent += $"[{fileName}]: ";
                 string content = await File.ReadAllTextAsync(textFile); // Buffered read
                 combinedContent += $"{content} \n";
             }
 
             // Write combine content to a new file
             string outputFileName = $"combined_text.txt";
-            string outputPath = Path.Combine($"{startingDirectory}", outputFileName);
+            string outputPath = Path.Combine($"{startingDirectory}\\Assets", outputFileName);
             await File.WriteAllTextAsync(outputPath, combinedContent); // Buffered write
 
             // Read new file by streaming method
